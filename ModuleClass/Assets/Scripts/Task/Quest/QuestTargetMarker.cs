dfcf617fd1933e9d86f6e076fe8df7de847be11c
sumin <dlatnals17@naver.com>
2024-03-25 17:50:53 +0900

    QuestTargetMarker

--- ModuleClass/Assets/Scripts/Task/Quest/QuestTargetMarker.cs
+++ ModuleClass/Assets/Scripts/Task/Quest/QuestTargetMarker.cs
@@ -16,14 +16,67 @@ public class QuestTargetMarker : MonoBehaviour
 
     private int currentRunningTargetTaskCount;
 
-    [System.Serializable]
-    private struct MarkerMaterialData
+    private void Awake()
     {
-        public Category category;
-        public Material markerMaterial;
+        cameraTransform = Camera.main.transform;
+        renderer = GetComponent<Renderer>();
     }
 
-    private void UpdateRunningTargetTaskCount(Task task, TaskState currentState, TaskState prevState)
+    private void Start()
+    {
+        gameObject.SetActive(false);
+
+        QuestSystem.Instance.onQuestRegistered += TryAddTargetQuest;
+        foreach (var quest in QuestSystem.Instance.ActiveQuests)
+        {
+            TryAddTargetQuest(quest);
+        }
+    }
+
+    private void Update()
+    {
+        var rotation = Quaternion.LookRotation((cameraTransform.position - transform.position).normalized);
+        transform.rotation = Quaternion.Euler(0f, rotation.eulerAngles.y + 180f, 0f);
+    }
+
+    private void OnDestroy()
+    {
+        QuestSystem.Instance.onQuestRegistered -= TryAddTargetQuest;
+        foreach ((Quest quest, Task task) in targetTasksByQuest)
+        {
+            quest.onNewTaskGroup -= UpdateTargetTask;
+            quest.onCompleted -= RemoveTargetQuest;
+            task.onStateChanged -= UpdateRunningTargetTaskCount;
+        }
+    }
+
+    private void TryAddTargetQuest(Quest quest)
+    {
+        if(target != null && quest.ContainsTarget(target))
+        {
+            quest.onNewTaskGroup += UpdateTargetTask;
+            quest.onCompleted += RemoveTargetQuest;
+
+            UpdateTargetTask(quest, quest.CurrentTaskGroup);
+        }
+    }
+
+    private void UpdateTargetTask(Quest quest, TaskGroup currentTaskGroup, TaskGroup prevTaskGroup = null)
+    {
+        targetTasksByQuest.Remove(quest);
+        var task = currentTaskGroup.FindTaskByTarget(target);
+        if(task != null)
+        {
+            targetTasksByQuest[quest] = task;
+            task.onStateChanged += UpdateRunningTargetTaskCount;
+
+            UpdateRunningTargetTaskCount(task, task.State);
+        }
+    }
+
+    private void RemoveTargetQuest(Quest quest) => targetTasksByQuest.Remove(quest);
+
+    private void UpdateRunningTargetTaskCount(Task task, TaskState currentState, TaskState prevState = TaskState.Inactive)
     {
         if (currentState == TaskState.Running)
         {
@@ -34,5 +87,12 @@ public class QuestTargetMarker : MonoBehaviour
             currentRunningTargetTaskCount--;
 
         gameObject.SetActive(currentRunningTargetTaskCount != 0);
+    }
+
+    [System.Serializable]
+    private struct MarkerMaterialData
+    {
+        public Category category;
+        public Material markerMaterial;
     }
 }
